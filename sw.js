2db1451b368f9c35234bf5abd817834d487db4ec
phummiphat582-debug <phummiphat582@gmail.com>
2025-12-27 18:17:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  self.clients.claim();
-});
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  const url = new URL(e.request.url);
+
+  // ❌ อย่าไปดัก request ของ Firebase / Google
+  if (
+    url.origin.includes('googleapis.com') ||
+    url.origin.includes('gstatic.com')
+  ) {
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
